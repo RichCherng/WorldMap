d21eda489eeff7f70428899c24e5fc5cceb3db4a
Rich Cherngchaosil <richcherng@gmail.com>
2025-10-27 16:28:17 -0700

    Print out local address

--- src/main/java/com/worldmap/WorldMapApplication.java
+++ src/main/java/com/worldmap/WorldMapApplication.java
@@ -2,11 +2,31 @@ package com.worldmap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.env.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @SpringBootApplication
 public class WorldMapApplication {
 
+    @Autowired
+    private Environment environment;
+
     public static void main(String[] args) {
         SpringApplication.run(WorldMapApplication.class, args);
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void onApplicationReady() {
+        String port = environment.getProperty("server.port", "8080");
+        String contextPath = environment.getProperty("server.servlet.context-path", "");
+        
+        System.out.println("\n" + "=".repeat(60));
+        System.out.println("🌍 WorldMap Application Started Successfully!");
+        System.out.println("=".repeat(60));
+        System.out.println("🔗 Local Address: http://localhost:" + port + contextPath);
+        System.out.println("🔗 Network Address: http://0.0.0.0:" + port + contextPath);
+        System.out.println("=".repeat(60) + "\n");
     }
 }
